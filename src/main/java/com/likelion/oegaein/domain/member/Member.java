af34c75b5ce203e93aba26157b2b4b84522bdc31
정재웅 <jweidon0821@gmail.com>
2024-01-06 15:50:32 +0900

    feat(member): 회원가입 api 컨트롤러 세팅

--- src/main/java/com/likelion/oegaein/domain/member/Member.java
+++ src/main/java/com/likelion/oegaein/domain/member/Member.java
@@ -5,13 +5,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import org.springframework.transaction.annotation.Transactional;
 
 @Entity
 @Getter
+@Setter
 @Builder
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 @AllArgsConstructor
 @Transactional
 public class Member extends BaseTime {
--- src/main/java/com/likelion/oegaein/repository/member/MemberRepository.java
+++ src/main/java/com/likelion/oegaein/repository/member/MemberRepository.java
@@ -5,18 +5,24 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
 
     private final EntityManager em;
 
-    public Long save(Member user){
-        em.persist(user);
-        return user.getId();
+    public void save(Member member) {
+        em.persist(member);
     }
 
-    public Member find(Long id){
+    public Member findById(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
     }
 }
