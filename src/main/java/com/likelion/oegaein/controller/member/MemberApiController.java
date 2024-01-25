3303e3dc92b23ede67a324433f6bcdf46dd75e23
정재웅 <jweidon0821@gmail.com>
2024-01-25 18:28:52 +0900

    Apply .gitignore
    - 원격 저장소에 수정된 gitignore 반영 (application.yaml 추가)

--- src/main/java/com/likelion/oegaein/controller/member/MemberApiController.java
+++ src/main/java/com/likelion/oegaein/controller/member/MemberApiController.java
@@ -4,12 +4,17 @@ import com.likelion.oegaein.domain.member.Member;
 import com.likelion.oegaein.service.member.MemberService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +22,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberApiController {
 
     private final MemberService memberService;
+    
+    @GetMapping("api/v1/member")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("api/v2/member")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getEmail()))
+                .collect(Collectors.toList());
+        
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String email;
+    }
 
     @PostMapping("api/v1/member/signup")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
