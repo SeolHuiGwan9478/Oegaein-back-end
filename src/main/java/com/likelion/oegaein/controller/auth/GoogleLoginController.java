a25d0f1e1fa350c96357103f403ad62018c2cc8c
정재웅 <jweidon0821@gmail.com>
2024-02-04 02:28:56 +0900

    Fix(auth): Service.access 로직 누락 수정 + HTTP Mapping 어노테이션 적용

--- src/main/java/com/likelion/oegaein/controller/auth/GoogleLoginController.java
+++ src/main/java/com/likelion/oegaein/controller/auth/GoogleLoginController.java
@@ -1,19 +1,9 @@
 package com.likelion.oegaein.controller.auth;
 
-import com.likelion.oegaein.dto.auth.GoogleInfResponse;
-import com.likelion.oegaein.dto.auth.GoogleRequest;
-import com.likelion.oegaein.dto.auth.GoogleResponse;
 import com.likelion.oegaein.service.auth.GoogleLoginService;
-import com.likelion.oegaein.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,20 +11,19 @@ import java.util.Map;
 @CrossOrigin("*")
 public class GoogleLoginController {
     private final GoogleLoginService googleLoginService;
-    @RequestMapping(value = "/api/v1/oauth2/google/login", method = RequestMethod.POST)
-
+    @PostMapping("/api/v1/oauth2/google/login")
     public String loginUrlGoogle() {
         return googleLoginService.requestUrl();
     }
 
-    @RequestMapping(value = "/api/v1/oauth2/google/login", method = RequestMethod.GET)
-    public String loginGoogle(@RequestParam(value = "code") String authCode) {
+    @GetMapping("/api/v1/oauth2/google/login")
+    public void loginGoogle(@RequestParam(value = "code") String authCode) {
         String email = googleLoginService.access(authCode);
         if (googleLoginService.isHufsEmail(email)) {
-            return email;
+            log.info(email);
+            googleLoginService.access(authCode);
         } else {
             log.info("Not HUFS Email");
-            return "한국외국어대학교 메일로 가입해주세요";
         }
     }
 }
