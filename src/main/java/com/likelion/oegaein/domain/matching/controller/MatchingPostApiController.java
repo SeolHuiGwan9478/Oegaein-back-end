56a5acdb900a8733a4820729812d3321350f6989
SeolHuiGwan9478 <shg5977@naver.com>
2024-03-08 11:58:31 +0900

    Refactor: 매칭글 조회, 제거, 수정 관련 반환 데이터 리팩토링 완료

--- src/main/java/com/likelion/oegaein/domain/matching/controller/MatchingPostApiController.java
+++ src/main/java/com/likelion/oegaein/domain/matching/controller/MatchingPostApiController.java
@@ -3,6 +3,7 @@ package com.likelion.oegaein.domain.matching.controller;
 import com.likelion.oegaein.domain.matching.dto.matchingpost.*;
 import com.likelion.oegaein.domain.matching.service.MatchingPostService;
 import com.likelion.oegaein.domain.member.service.MemberService;
+import com.likelion.oegaein.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -16,14 +17,14 @@ public class MatchingPostApiController {
     private final MatchingPostService matchingPostService;
 
     @GetMapping("/api/v1/matchingposts") // 전체 매칭 글 조회
-    public ResponseEntity<FindMatchingPostsResponse> getMatchingPosts(){
+    public ResponseEntity<ResponseDto> getMatchingPosts(){
         log.info("Request to get matchingposts"); // logging
         FindMatchingPostsResponse response = matchingPostService.findAllMatchingPosts();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PostMapping("/api/v1/matchingposts") // 매칭 글 등록
-    public ResponseEntity<CreateMatchingPostResponse> postMatchingPost(@RequestBody CreateMatchingPostRequest dto){
+    public ResponseEntity<ResponseDto> postMatchingPost(@RequestBody CreateMatchingPostRequest dto){
         log.info("Request to post matchingpost");
         CreateMatchingPostData convertedDto = CreateMatchingPostData.toCreateMatchingPostData(dto);
         CreateMatchingPostResponse response = matchingPostService.saveMatchingPost(convertedDto);
@@ -31,54 +32,31 @@ public class MatchingPostApiController {
     }
 
     @GetMapping("/api/v1/matchingposts/{matchingpostid}") // 특정 매칭 글 조회
-    public ResponseEntity<FindMatchingPostResponse> getMatchingPost(@PathVariable("matchingpostid") Long matchingPostId){
+    public ResponseEntity<ResponseDto> getMatchingPost(@PathVariable("matchingpostid") Long matchingPostId){
         log.info("Request to get matchingpost by id-{}", matchingPostId);
-        try{
-            FindMatchingPostResponse response = matchingPostService.findByIdMatchingPost(matchingPostId);
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        }catch (IllegalArgumentException e){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        FindMatchingPostResponse response = matchingPostService.findByIdMatchingPost(matchingPostId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("/api/v1/matchingposts/{matchingpostid}")
-    public ResponseEntity<Long> deleteMatchingPost(@PathVariable("matchingpostid") Long matchingPostId){
+    public ResponseEntity<ResponseDto> deleteMatchingPost(@PathVariable("matchingpostid") Long matchingPostId){
         log.info("Request to delete matchingpost by id-{}", matchingPostId);
-        try{
-            matchingPostService.removeMatchingPost(matchingPostId);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }catch (IllegalArgumentException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        matchingPostService.removeMatchingPost(matchingPostId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping("/api/v1/matchingposts/{matchingpostid}")
     public ResponseEntity<UpdateMatchingPostResponse> putMatchingPost(@PathVariable("matchingpostid") Long matchingPostId,
                                                   @RequestBody UpdateMatchingPostRequest dto){
-        try{
-            UpdateMatchingPostData convertedDto = UpdateMatchingPostData.builder()
-                    .title(dto.getTitle())
-                    .content(dto.getContent())
-                    .deadline(dto.getDeadline())
-                    .dongType(dto.getDongType())
-                    .roomSizeType(dto.getRoomSizeType())
-                    .build();
-            Long updatedMatchingPost = matchingPostService.updateMatchingPost(matchingPostId, convertedDto);
-            UpdateMatchingPostResponse response = new UpdateMatchingPostResponse(updatedMatchingPost);
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        }catch(IllegalArgumentException e){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        UpdateMatchingPostData convertedDto = UpdateMatchingPostData.toUpdateMatchingPostData(dto);
+        UpdateMatchingPostResponse response = matchingPostService.updateMatchingPost(matchingPostId, convertedDto);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/api/v1/my-matchingposts")
     public ResponseEntity<FindMyMatchingPostResponse> getMyMatchingPosts(){
         log.info("Request to get my matching posts");
-        try{
-            FindMyMatchingPostResponse response = matchingPostService.findMyMatchingPosts();
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        }catch (Exception e){ // 에러 수정 필요
-            return new ResponseEntity<>(HttpStatus.NON_AUTHORITATIVE_INFORMATION);
-        }
+        FindMyMatchingPostResponse response = matchingPostService.findMyMatchingPosts();
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
