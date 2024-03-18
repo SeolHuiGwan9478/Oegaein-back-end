e22c10ead336d4a040aab1f1976bac0264dc6f10
SeolHuiGwan9478 <shg5977@naver.com>
2024-03-18 12:18:30 +0900

    Feat: 채팅방 나가기 기능 구현 완료

--- src/main/java/com/likelion/oegaein/domain/chat/dto/CreateOneToOneChatRoomRequest.java
+++ src/main/java/com/likelion/oegaein/domain/chat/dto/CreateOneToOneChatRoomRequest.java
@@ -2,8 +2,6 @@ package com.likelion.oegaein.domain.chat.dto;
 
 import lombok.Getter;
 
-import java.util.List;
-
 @Getter
 public class CreateOneToOneChatRoomRequest {
     private Long opponentMemberId; // 상대방 ID
